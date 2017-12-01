d06c761a02103d8acd51b40f11f9c4436ed79f42
mattsald48 <matt.saldana8@gmail.com>
2017-11-30 19:02:14 -0600

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -26,6 +26,15 @@ app.post('/todos', (req, res) => {
   })
 });
 
+//get route
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+	  res.send({todos})
+	}, (e) => {
+      res.status(400).send(e);
+	})
+});
+
 
 //server start
 app.listen(3000, () =>{
